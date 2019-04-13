aa7539d4c10fbbca8d72f74d58e55bcc2ae55b85
awindahl <awindahl@kth.se>
2019-04-13 14:22:57 +0200

    added spaceship movement

--- Wild Jam 8/scripts/SpaceShip.gd
+++ Wild Jam 8/scripts/SpaceShip.gd
@@ -1,44 +1,49 @@
 extends KinematicBody
 
-const ACCELERATION = 0.5
-const DECELERATION = 0.5
+const ACCELERATION = 1
+const DECELERATION = 1
 
 
 onready var cameraLook = get_node("CameraLookat")
 onready var camera = get_node("CameraLookat/Camera")
+onready var model = get_node("Model")
+onready var collider = get_node("CollisionShape")
 
 var direction2D = Vector2()
 var direction3D = Vector3()
-var moveSpeed = 200
+var moveSpeed = 50
 var velocity = Vector3()
 var yaw = 0
 var pitch = 0
-var ViewSensitivity = 0.2
+var ViewSensitivity = 0.05
+var forward
+var backward
+var left
+var right
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	#camera.set_as_toplevel(true)
 
 func _process(delta):
-	var forward = Input.is_action_pressed("forward")
-	var backward = Input.is_action_pressed("backward")
-	var left = Input.is_action_pressed("left")
-	var right = Input.is_action_pressed("right")
+	forward = Input.is_action_pressed("forward")
+	backward = Input.is_action_pressed("backward")
+	left = Input.is_action_pressed("left")
+	right = Input.is_action_pressed("right")
+	var aim = camera.get_camera_transform().basis
+	var value = 0
 	
-	if forward:
-		pass
-	if backward:
-		pass
-	if left:
-		pass
-	if right:
-		pass
-	
-	direction2D = Vector2()
 	direction3D = Vector3()
 	
-	direction2D = direction2D.normalized()
-	direction3D += global_transform.basis.z.normalized() * direction2D.y
-	direction3D += global_transform.basis.x.normalized() * direction2D.x
+	if forward:
+		direction3D -= model.get_transform().basis[2]
+	if backward:
+		direction3D += model.get_transform().basis[2]
+	if left:
+		direction3D -= model.get_transform().basis[0]
+	if right:
+		direction3D += model.get_transform().basis[0]
+	
 	
 	## Acceleration and Retardation
 	var hVel = velocity
@@ -53,13 +58,14 @@ func _process(delta):
 	hVel = hVel.linear_interpolate(target, acceleration * moveSpeed * delta)
 	velocity.x = hVel.x
 	velocity.z = hVel.z
-	
-	# Only move if camera is not rotating and not rooted and not zoomed
+	velocity.y = hVel.y
+
 	velocity = move_and_slide(velocity)
+	model.rotation = model.rotation.linear_interpolate(cameraLook.rotation, delta)
 	
 
 func _unhandled_input(event):
 	if event is InputEventMouseMotion:
 		yaw = yaw - event.relative.x * ViewSensitivity
-		pitch = max(min(pitch - event.relative.y * ViewSensitivity, 90), -50)
+		pitch = pitch - event.relative.y * ViewSensitivity
 		cameraLook.rotation = Vector3(deg2rad(pitch), deg2rad(yaw), 0)
