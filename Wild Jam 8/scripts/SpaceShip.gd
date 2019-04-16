35bc06e46f551b08132301276bb429675e803dc5
awindahl <awindahl@kth.se>
2019-04-16 10:44:13 +0200

    updated ship controls

--- Wild Jam 8/scripts/SpaceShip.gd
+++ Wild Jam 8/scripts/SpaceShip.gd
@@ -1,17 +1,22 @@
 extends KinematicBody
 
-const ACCELERATION = 1
+const ACCELERATION = 4
 const DECELERATION = 1
-
+const MAXSPEED = 100.0
+const MINSPEED = 0.0
 
 onready var cameraLook = get_node("CameraLookat")
 onready var camera = get_node("CameraLookat/Camera")
 onready var model = get_node("Model")
 onready var collider = get_node("CollisionShape")
+onready var exhaust = get_node("Model/Particles")
+onready var exhaust2 = get_node("Model/Particles2")
+onready var exhaust3 = get_node("Model/Particles3")
 
 var direction2D = Vector2()
 var direction3D = Vector3()
-var moveSpeed = 50
+var moveSpeed = 0.0
+var rotationSpeed = 1
 var velocity = Vector3()
 var yaw = 0
 var pitch = 0
@@ -23,6 +28,7 @@ var right
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	#Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	#camera.set_as_toplevel(true)
 
 func _process(delta):
@@ -32,41 +38,50 @@ func _process(delta):
 	right = Input.is_action_pressed("right")
 	var aim = camera.get_camera_transform().basis
 	var value = 0
-	
-	direction3D = Vector3()
+	rotationSpeed = 1
+	#direction3D = Vector3()
+	direction3D = -model.get_transform().basis[2]
 	
 	if forward:
-		direction3D -= model.get_transform().basis[2]
+		moveSpeed = lerp(moveSpeed, MAXSPEED, 0.01)
+		exhaust.visible = true
+		exhaust2.visible = true
+		exhaust3.visible = true
 	if backward:
-		direction3D += model.get_transform().basis[2]
+		#direction3D += model.get_transform().basis[2]
+		moveSpeed = lerp(moveSpeed, MINSPEED, 0.04)
+		exhaust.visible = false
+		exhaust2.visible = false
+		exhaust3.visible = false
 	if left:
-		direction3D -= model.get_transform().basis[0]
-		if model.rotation_degrees.z < 45:
-			model.rotation_degrees.z += 1
+		#direction3D -= model.get_transform().basis[0]
+		#if model.rotation_degrees.z < 90:
+		model.rotation_degrees.z += 1 * moveSpeed/50
+		rotationSpeed = 2
 	if right:
-		direction3D += model.get_transform().basis[0]
-		if model.rotation_degrees.z < -45:
-			model.rotation_degrees.z -= 1
+		#direction3D += model.get_transform().basis[0]
+		#if model.rotation_degrees.z > -90:
+		model.rotation_degrees.z -= 1 * moveSpeed/50
+		rotationSpeed = 2
 	
+	if moveSpeed < 2 and not forward:
+		moveSpeed = 0
+		exhaust.visible = false
 	
-	## Acceleration and Retardation
-	var hVel = velocity
-	hVel.y = 0
-	var target = direction3D * moveSpeed
-	var acceleration
-	if direction3D.dot(hVel) > 0:
-		acceleration = ACCELERATION
-	else:
-		acceleration = DECELERATION
+	print($AudioStreamPlayer.playing)
+	$AudioStreamPlayer.volume_db = (moveSpeed/12) - 15
 	
-	hVel = hVel.linear_interpolate(target, acceleration * moveSpeed * delta)
-	velocity.x = hVel.x
-	velocity.z = hVel.z
-	velocity.y = hVel.y
-
-	velocity = move_and_slide(velocity)
-	model.rotation = model.rotation.linear_interpolate(cameraLook.rotation, delta)
+	if exhaust.visible == true and $AudioStreamPlayer.playing == false:
+		$AudioStreamPlayer.play()
+		$AudioStreamPlayer.volume_db = moveSpeed/4
+	elif exhaust.visible == false:
+		$AudioStreamPlayer.stop()
+	 
+	velocity = move_and_slide(direction3D*moveSpeed)
 	
+	cameraLook.rotation.z = 0
+	var rotationTarget = model.rotation.linear_interpolate(cameraLook.rotation, delta * rotationSpeed)
+	model.rotation = rotationTarget
 
 func _unhandled_input(event):
 	if event is InputEventMouseMotion:
